2760d0354dc16a2d74da55e6c7df3c8c8c11a9d8
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-08-26 22:52:28 +0530

    update:update in code

---
+++ Backend/index.js
@@ -0,0 +1,121 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mysql2 from "mysql2";
+import multer from "multer";
+import fs from "fs";
+//import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+//mongoose.connect("mongodb://127.0.0.1/CodemapcomunityTesting");
+
+const conn = mysql2.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "AritraCoder@2003",
+  database: "codemapcommunity",
+  port: 3306, //3001
+});
+
+app.get("/", (req, res) => {
+  const myobj = {
+    name: "Aritra",
+    age: 21,
+    college: "IIMT",
+  };
+  res.send(myobj);
+});
+
+app.patch("/user/update", (req, res) => {
+  const email = req.body.email;
+  const college = req.body.college;
+  const sql = `UPDATE user SET college='${college}' WHERE email='${email}'`;
+  conn.query(sql, (err, data) => {
+    if (err) console.log(err);
+    else return res.status(200).send({ status: 200 });
+  });
+});
+
+app.delete("/user/delete/:id", (req, res) => {
+  const email = req.query.email;
+  const sql = `DELETE FROM user WHERE email='${email}'`;
+  conn.query(sql, (err, data) => {
+    if (err) console.log(err);
+    else return res.status(200).send({ status: 200 });
+  });
+});
+
+app.post("/user", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const college = req.body.college;
+
+  const sql = "INSERT INTO user (name,email,college,password) VALUES(?,?,?,?);";
+  conn.query(sql, [name, email, password, college], (err, data) => {
+    if (err) console.log(err);
+    else {
+      return res.status(200).send({ status: 200 });
+    }
+  });
+});
+
+app.get("/user", (req, res) => {
+  const sql = "SELECT * FROM user;";
+  conn.query(sql, (err, data) => {
+    if (err) console.log(err);
+    return res.json(data);
+  });
+});
+
+/*app.get("/user", (req, res) => {
+  users
+    .find()
+    .then((val) => {
+      res.json(val);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});*/
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var dir = "./uploads";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.post("/fileUpload", upload.single("file"), (req, res) => {
+  const filename = req.file.filename;
+  const type = req.body.doc;
+  const sql = "INSERT INTO document (title,image) VALUES(?,?);";
+  conn.query(sql, [type, filename], (err, data) => {
+    if (err) console.log(err);
+    else {
+      return res.status(200).send({ status: 200 });
+    }
+  });
+});
+app.get("/fileUpload", (req, res) => {
+  const sql = "SELECT * FROM document;";
+  conn.query(sql, (err, data) => {
+    if (err) console.log(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Backend Connected");
+});
---
+++ Client/my-project/src/App.jsx
@@ -0,0 +1,232 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [state, setState] = useState({
+    selectedFile: null,
+    filename: null,
+  });
+  const fileSelectedHandler = (event) => {
+    let file = event.target.files[0].name;
+    setState({
+      selectedFile: event.target.files[0],
+      filename: document.getElementById("file").value,
+    });
+    console.log(file);
+  };
+
+  const uploadData = (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const college = document.getElementById("college").value;
+    const password = document.getElementById("password").value;
+    axios
+      .post(`${import.meta.env.VITE_APP_API_URL}` + "user", {
+        name: name,
+        email: email,
+        college: college,
+        password: password,
+      })
+      .then((res) => {
+        if (res.data.status === 200) {
+          alert("Data posted successfully");
+        } else {
+          alert("Something went wrong");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const uploadDataDelete = (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email3").value;
+    axios
+      .delete(`${import.meta.env.VITE_APP_API_URL}data?email=${email}`, {
+        email: email,
+      })
+      .then((res) => {
+        if (res.data.status === 200) {
+          alert("Data deleted successfully");
+        } else {
+          alert("Something went wrong");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const uploadData1 = (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email2").value;
+    const college1 = document.getElementById("college1").value;
+
+    axios
+      .patch(`${import.meta.env.VITE_APP_API_URL}user/update`, {
+        email: email,
+        college: college1,
+      })
+      .then((res) => {
+        if (res.data.status === 200) {
+          alert("Data Changed successfully");
+        } else {
+          alert("Something went wrong");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const uploadFile = (e) => {
+    e.preventDefault();
+    let doc = document.getElementById("doc").value;
+
+    let formData = new FormData();
+    formData.append("doc", doc);
+    formData.append("filename", state.filename);
+    formData.append("file", state.selectedFile);
+
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+    };
+    let control = true;
+    axios
+      .post(`${import.meta.env.VITE_APP_API_URL}fileUpload`, formData, config)
+      .then((res) => {
+        if (res.data.status === 200) {
+          alert("File uploaded Successfully");
+        } else {
+          alert("Upload not successful");
+        }
+      });
+  };
+  return (
+    <>
+      <p className="font-bold  text-3xl">Please Fill the details</p>
+      <form className="mt-8">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter name...."
+        />
+        <br></br>
+        <br></br>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter email...."
+        />
+        <br></br>
+        <br></br>
+        <input
+          type="text"
+          id="college"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter college...."
+        />
+        <br></br>
+        <br></br>
+        <input
+          type="password"
+          id="password"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter password...."
+        />
+        <br></br> <br></br>
+        <button className="bg-black text-white p-2" onClick={uploadData}>
+          Submit
+        </button>
+      </form>
+
+      <p className="font-bold  text-3xl mt-[6vmin]">
+        Please Fill the details to update...
+      </p>
+      <form className="mt-5">
+        <br></br>
+        <br></br>
+        <input
+          type="email"
+          name="email"
+          id="email2"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter email...."
+        />
+        <br></br>
+        <br></br>
+        <input
+          type="text"
+          id="college1"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter college...."
+        />
+        <br></br>
+        <br></br>
+        <button className="bg-black text-white p-2" onClick={uploadData1}>
+          Submit
+        </button>
+      </form>
+
+      <p className="font-bold  text-3xl mt-[6vmin]">
+        Please Fill the email to Delete...
+      </p>
+      <form className="mt-5">
+        <br></br>
+        <br></br>
+        <input
+          type="email"
+          name="email"
+          id="email3"
+          className="w-[40vmin] border-[2px] p-2"
+          placeholder="Enter email...."
+        />
+        <br></br>
+        <br></br>
+
+        <button className="bg-black text-white p-2" onClick={uploadDataDelete}>
+          Submit
+        </button>
+
+        <p className="font-bold  text-3xl mt-[6vmin]">
+          Please upload the file...
+        </p>
+        <form className="mt-5">
+          <br></br>
+          <br></br>
+          <input
+            type="text"
+            id="doc"
+            className="w-[40vmin] border-[2px] p-2"
+            placeholder="Enter  Doc type...."
+          />
+          <br></br>
+          <br></br>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            onChange={fileSelectedHandler}
+            className="w-[40vmin] border-[2px] p-2"
+            placeholder="Insert your file..."
+          />
+          <br></br>
+          <br></br>
+
+          <button className="bg-black text-white p-2" onClick={uploadFile}>
+            Submit
+          </button>
+        </form>
+      </form>
+    </>
+  );
+}
+
+export default App;
